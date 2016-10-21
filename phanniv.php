d358d6c124f3e43c967384a09710afcc705014e2
ponsfrilus <ponsfrilus@gmail.com>
2016-10-21 16:23:52 +0200

    getDynCSVData: get the CSV data regardless of number of headers

--- phanniv.php
+++ phanniv.php
@@ -1,29 +1,38 @@
 <?php
-
+/*
+ * Get data from the ./data.csv file
+*/
 function getCSVData()
 {
     $csvData = array_map('str_getcsv', file('data.csv'));
-    $csvHeader = $csvData[0];
+    $csvHeader = array_shift($csvData);
     $data = array();
-    array_shift($csvData);
     foreach ($csvData as $k => $v) {
         $data[] = array(
             $csvHeader[0] => $v[0],
             $csvHeader[1] => $v[1],
             $csvHeader[2] => $v[2],
-            $csvHeader[3] => $v[3], );
+            $csvHeader[3] => $v[3]);
     }
-
     return $data;
 }
 
-function getTodayISO8601()
+/* Same as getCSVData() but works if number of header change */
+function getDynCSVData()
 {
-    return date('Y-m-d');
+    $csvData = array_map('str_getcsv', file('data.csv'));
+    $csvHeader = array_shift($csvData);
+    $retData = array();
+    foreach ($csvData as $k => $v) {
+        foreach ($csvHeader as $kh => $kv) {
+            $retData[$k][$kv] = $v[$kh];
+        }
+    }
+    return $retData;
 }
 
 /*
- * Retourne la liste des personnes qui ont un anniversaire aujourd'hui
+ * Retourne la liste des personnes qui ont leur anniversaire aujourd'hui
  */
 function checkAnniversaires($data)
 {
@@ -31,6 +40,7 @@ function checkAnniversaires($data)
     $todayMonth = date('m');
     $todayDay = date('d');
     foreach ($data as $key => $value) {
+        /* Testing if month + day match today */
         if ((substr($value['DateISO'], 5, 2) == $todayMonth)
             && (substr($value['DateISO'], -2) == $todayDay)) {
             $bdToday[] = $value;
@@ -39,6 +49,9 @@ function checkAnniversaires($data)
     return $bdToday;
 }
 
+/* This is where the mails are sent.
+ * @todo: use a mail library, e.g. http://swiftmailer.org/
+*/
 function announceAnniversary($recipients, $happyGuys)
 {
     $to = 'nicolas.borboen@epfl.ch';
@@ -55,26 +68,31 @@ function announceAnniversary($recipients, $happyGuys)
     mail($to, $subject, $message, $headers);
 }
 
+/*
+* If someone has his birthday, email all the other persons
+* @Todo, @bug: if two people get their birthdays the same day,
+               both should received the mail for the other one...
+*/
 function getRecipients($all, $birthDayGuys)
 {
     $recipients = $all;
     foreach ($birthDayGuys as $key => $value) {
-        //$test = array_search($value["email"],$birthDayGuys);
         $toRemove = array_search($value['email'], array_column($all, 'email'));
         if (is_int($toRemove)) {
             unset($recipients[$toRemove]);
         }
     }
-
     return array_column($recipients, 'email');
 }
 
+/* In case we use phanniv.php in Command Line Interface */
 if (php_sapi_name() == 'cli') {
-    $everyOne = getCSVData();
-    announceAnniversary(getRecipients($everyOne, checkAnniversaires($everyOne)), checkAnniversaires($everyOne));
-    print_r(checkAnniversaires($everyOne));
+    print_r(getDynCSVData());
+    //announceAnniversary(getRecipients($everyOne, checkAnniversaires($everyOne)), checkAnniversaires($everyOne));
+    //print_r(checkAnniversaires($everyOne));
 } else {
-    ?>
+    /* For any other use case but CLI */
+?>
 
 <!doctype html>
 <html>
@@ -85,10 +103,12 @@ if (php_sapi_name() == 'cli') {
     <body>
         <pre>
             <?php
-            print_r(getCSVData());
-    print_r(checkAnniversaires(getCSVData())); ?>
+            print_r(getDynCSVData());
+            ?>
         </pre>
     </body>
 </html>
+
 <?php
-} ?>
+}
+?>
